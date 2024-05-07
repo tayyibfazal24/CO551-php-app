20ed9e4f7ba7f70bcbe7d31a9e7f74ca46e0f142
tayyibfazal23 <tayyibfazal23@gmail.com>
2024-05-07 15:34:54 +0100

    update

--- templates/partials/nav.php
+++ templates/partials/nav.php
@@ -1,6 +1,6 @@
 
 <div id="main-nav">
-
+   
 </div>
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
@@ -13,10 +13,20 @@
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="modules.php">My Modules</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="assignmodule.php">Assign Module</a>
+        </li>
+        <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="details.php">My Details</a>
+        </li>  
+        <li class="nav-item">  
           <a class="nav-link active" aria-current="page" href="students.php">Students</a>
+        </li>  
+        <li class="nav-item">  
           <a class="nav-link active" aria-current="page" href="addstudent.php">Add a student</a>
+        </li>  
+        <li class="nav-item">  
           <a class="nav-link active" aria-current="page" href="logout.php">Logout</a>
         </li>
       </form>
